82e1ae13f224919ff975d7b5ce0becb423211baa
seanaedmiston <seane999@gmail.com>
2013-05-01 00:43:19 +1000

    uwsgi-gevent

---
+++ uwsgi-gevent/hello.py
@@ -0,0 +1,15 @@
+import ujson
+
+
+def app(environ, start_response):
+    response = {
+      "message": "Hello, World!"
+    }
+    data = ujson.dumps(response)
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data))),
+        ('Connection', 'close')
+    ]
+    start_response('200 OK', response_headers)
+    return [data]
---
+++ uwsgi-gevent/setup.py
@@ -0,0 +1,21 @@
+
+import subprocess
+import sys
+import setup_util
+import os
+
+def start(args):
+  subprocess.Popen("uwsgi  -s /tmp/uwsgi.sock -w hello:app --http :8080 --master --gevent " + str((args.max_threads * 2)) + " ", shell=True, cwd="uwsgi-gevent")
+  return 0
+def stop():
+  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+  out, err = p.communicate()
+  for line in out.splitlines():
+    if 'wsgi.sock' in line:
+      try:
+        pid = int(line.split(None, 2)[1])
+        os.kill(pid, 9)
+      except OSError:
+        pass
+
+  return 0
