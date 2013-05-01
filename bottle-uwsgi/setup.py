7d8dd97c7bceec3635602527e77c40f06f186bea
seanaedmiston <seane999@gmail.com>
2013-05-01 12:14:33 +1000

    Move gunicorn-gevent into -gun-gevent so doesnt clash with upstreams meinhelm and add uwsgi (with gevent) for flask and bottle

---
+++ bottle-uwsgi/setup.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import setup_util
+import os
+
+def start(args):
+  setup_util.replace_text("bottle/app.py", "DBHOSTNAME", args.database_host)
+  subprocess.Popen("uwsgi -L -s /tmp/uwsgi.sock -w app:app --http :8080 --master --gevent " + str((args.max_threads * 2)) + " ", shell=True, cwd="bottle-uwsgi")
+  return 0
+
+def stop():
+  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+  out, err = p.communicate()
+  for line in out.splitlines():
+    if 'wsgi.sock' in line:
+      try:
+        pid = int(line.split(None, 2)[1])
+        os.kill(pid, 9)
+      except OSError:
+        pass
+
+  return 0
